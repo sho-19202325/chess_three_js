1f10c86b076a142737e40219b62ee6fb6bfd1485
Shota Kuchiki <shotakuchiki@ShotanoMacBook-Pro.local>
2022-11-26 15:04:38 +0900

    Fix ChessField to render all pieces

--- src/components/ChessField/index.tsx
+++ src/components/ChessField/index.tsx
@@ -1,5 +1,11 @@
+import { PieceProps } from "../../common/types"
+import { Bishop } from "../Bishop"
 import { ChessBoard } from "../ChessBoard"
+import { King } from "../King"
+import { Knight } from "../Knight"
+import { Luke } from "../Luke"
 import { Pawn } from "../Pawn"
+import { Queen } from "../Queen"
 
 export const ChessField = () => {
   return (
@@ -13,6 +19,14 @@ export const ChessField = () => {
       <Pawn place={[6, 2]} />
       <Pawn place={[7, 2]} />
       <Pawn place={[8, 2]} />
+      <Luke place={[1, 1]} />
+      <Luke place={[8, 1]} />
+      <Bishop place={[2, 1]} />
+      <Bishop place={[7, 1]} />
+      <Knight place={[3, 1]} />
+      <Knight place={[6, 1]} />
+      <Queen place={[4, 1]} />
+      <King place={[5, 1]} />
     </>
   )
 }
