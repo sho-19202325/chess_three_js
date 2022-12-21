db128d8f51ec34df41d05a9f0ca6479c5c53794d
Shota Kuchiki <shotakuchiki@ShotanoMacBook-Pro.local>
2022-12-21 19:19:56 +0900

    Fix Square to render available places

--- src/components/Square/index.tsx
+++ src/components/Square/index.tsx
@@ -1,21 +1,62 @@
-import { Suspense } from "react"
-import { XyzSpace } from "../../common/types"
+import { ThreeEvent, useFrame } from "@react-three/fiber"
+import { calculatePositionFromPlace } from "common/utils"
+import { FieldContext } from "contexts/FieldContext"
+import { selectSquare } from "pages/chessField/actions"
+import { Suspense, useContext, useEffect, useRef, useState } from "react"
+import { MeshStandardMaterial, MeshToonMaterial } from "three"
+import { Place } from "../../common/types"
 
 type SquareProps = Readonly<{
-  position: XyzSpace
+  place: Place
   isBlack: boolean
+  isActive?: boolean
 }>
 
 const SQUARE_SIZE:[number, number] = [1, 1]
+const MAX_RGB = 1
+const DELTA_RGB = 0.01
 
-export const Square = ({ position, isBlack }: SquareProps) => {
-  const squareColor = isBlack ? '#000' : '#fff'
+export const Square = ({ place, isBlack, isActive }: SquareProps) => {
+  let squareColor = isBlack && !isActive ? '#000' : '#fff'
+  const material = useRef<MeshStandardMaterial>(null)
+  const position = calculatePositionFromPlace(place)
+  const { state, dispatch } = useContext(FieldContext)
+
+  const flashActiveSquare = () => {
+    if (material.current === null || !isActive) return
+
+    if (material.current.color.g <= 0 && material.current.color.b <= 0) {
+      material.current.color.g = MAX_RGB
+      material.current.color.b = MAX_RGB
+    } else {
+      material.current.color.g -= DELTA_RGB
+      material.current.color.b -= DELTA_RGB
+    }
+  }
+
+  const handleClickActiveSqure = (e: ThreeEvent<PointerEvent>) => {
+    if (!isActive || state.phase !== "SELECT_SQUARE") return
+    e.stopPropagation()
+
+    dispatch(selectSquare(state, place))
+  }
+
+  useEffect(() => {
+    // アクティブでなくなった時にcolorを元に戻す
+    if (!isActive && material.current !== null) {
+      material.current.color.set(squareColor)
+    }
+  }, [isActive, squareColor])
+
+  useFrame(() => {
+    flashActiveSquare()
+  })
 
   return (
     <Suspense fallback={null}>
-        <mesh position={position}>
+        <mesh position={position} onPointerDown={(e) => handleClickActiveSqure(e)}>
           <planeGeometry args={SQUARE_SIZE}  />
-          <meshStandardMaterial color={squareColor} />
+          <meshStandardMaterial opacity={0} ref={material} color={squareColor} />
         </mesh>
     </Suspense>
   )
