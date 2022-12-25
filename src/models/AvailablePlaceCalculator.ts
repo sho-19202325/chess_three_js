f029676023d53087d4918d616e24ececc9bbeb13
Shota Kuchiki <shotakuchiki@ShotanoMacBook-Pro.local>
2022-12-25 17:10:16 +0900

    Add class of AvailablePlaceCalculatorForLuke

--- src/models/AvailablePlaceCalculator.ts
+++ src/models/AvailablePlaceCalculator.ts
@@ -1,4 +1,6 @@
+import { HORIZONTAL_SQUARE_COUNT, VIRTICAL_SQUARE_COUNT } from "consts/chessBoard"
 import {  PieceInfo, Place, Player } from "types/common"
+import { isIncludeSamePlace } from "utils/place"
 
 export abstract class AvailablePlaceCalculator {
   piece: PieceInfo
@@ -14,4 +16,68 @@ export abstract class AvailablePlaceCalculator {
   }
 
   abstract run: () => Place[]
+
+  protected findAvailablePlacesInFront = (fromPlace: Place) => {
+    const availablePlaces:Place[] = []
+
+    for (let placeY = fromPlace[1] + 1; placeY <= VIRTICAL_SQUARE_COUNT; placeY++) {
+      const targetPlace:Place = [fromPlace[0], placeY]
+
+      if (isIncludeSamePlace(targetPlace, this.ownPlaces)) return availablePlaces
+
+      availablePlaces.push(targetPlace)
+
+      if (isIncludeSamePlace(targetPlace, this.opponentPlaces)) return availablePlaces
+    }
+
+    return availablePlaces
+  }
+
+  protected findAvailablePlacesInBack = (fromPlace: Place) => {
+    const availablePlaces:Place[] = []
+
+    for (let placeY = fromPlace[1] - 1; placeY >= 0; placeY--) {
+      const targetPlace:Place = [fromPlace[0], placeY]
+
+      if (isIncludeSamePlace(targetPlace, this.ownPlaces)) return availablePlaces
+
+      availablePlaces.push(targetPlace)
+
+      if (isIncludeSamePlace(targetPlace, this.opponentPlaces)) return availablePlaces
+    }
+
+    return availablePlaces
+  }
+
+  protected findAvailablePlacesInRight = (fromPlace: Place) => {
+    const availablePlaces:Place[] = []
+
+    for (let placeX = fromPlace[0] + 1; placeX <= HORIZONTAL_SQUARE_COUNT; placeX++) {
+      const targetPlace:Place = [placeX, fromPlace[1]]
+
+      if (isIncludeSamePlace(targetPlace, this.ownPlaces)) return availablePlaces
+
+      availablePlaces.push(targetPlace)
+
+      if (isIncludeSamePlace(targetPlace, this.opponentPlaces)) return availablePlaces
+    }
+
+    return availablePlaces
+  }
+
+  protected findAvailablePlacesInLeft = (fromPlace: Place) => {
+    const availablePlaces:Place[] = []
+
+    for (let placeX = fromPlace[0] - 1; placeX >= 0; placeX--) {
+      const targetPlace:Place = [placeX, fromPlace[1]]
+
+      if (isIncludeSamePlace(targetPlace, this.ownPlaces)) return availablePlaces
+
+      availablePlaces.push(targetPlace)
+
+      if (isIncludeSamePlace(targetPlace, this.opponentPlaces)) return availablePlaces
+    }
+
+    return availablePlaces
+  }
 }
--- src/models/AvailablePlaceCalculatorFactory.ts
+++ src/models/AvailablePlaceCalculatorFactory.ts
@@ -1,4 +1,5 @@
 import { PieceInfo, Place, Player } from "types/common"
+import { AvailablePlaceCalculatorForLuke } from "./AvailablePlaceCalculator/AvailablePlaceCalculatorForLuke"
 import { AvailablePlaceCalculatorForPawn } from "./AvailablePlaceCalculator/AvailablePlaceCalculatorForPawn"
 
 export class AvailablePlaceCalculatorFactory {
@@ -12,6 +13,8 @@ export class AvailablePlaceCalculatorFactory {
       case "Pawn":
         return new AvailablePlaceCalculatorForPawn(piece, player, ownPlaces, opponentPlaces)
       // TODO: 他の駒のロジックも実装する
+      case "Luke":
+        return new AvailablePlaceCalculatorForLuke(piece, player, ownPlaces, opponentPlaces)
       default:
         throw new Error(`"${piece.name}"はAvailablePlaceCalculatorFactoryに登録されていません。`)
     }
