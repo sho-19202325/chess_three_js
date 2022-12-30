5e4f223b29d03ce6b21178649e1a214a3a28a8ac
Shota Kuchiki <shotakuchiki@ShotanoMacBook-Pro.local>
2022-12-30 16:55:09 +0900

    Fix Piece enable to select when select target place

--- src/components/PlayerPieces/index.tsx
+++ src/components/PlayerPieces/index.tsx
@@ -1,21 +1,11 @@
-import { PieceInfo } from "types/common"
 import { Piece } from "components/Piece"
 import { FieldContext } from "contexts/FieldContext"
-import { selectPiece } from "pages/chessField/actions"
 import { useContext } from "react"
 import { Player } from "types/common"
 
 export const PlayerPieces = ({ player }: { player: Player }) => {
-  const { state, dispatch } = useContext(FieldContext)
+  const { state } = useContext(FieldContext)
   const ownPieces = state.playerPieces[player]
-
-  const handleSelectPiece = (piece: PieceInfo) => {
-    if (state.currentPlayer !== player ||
-        (state.phase !== "SELECT_PIECE" && state.phase !== "SELECT_SQUARE")
-    ) return
-
-    dispatch(selectPiece(state, piece))
-  }
 
   return (
     <>
@@ -24,7 +14,6 @@ export const PlayerPieces = ({ player }: { player: Player }) => {
                  key={index}
                  piece={item}
                  player={player}
-                 handleSelectPiece={handleSelectPiece}
                />
       })}
     </>
